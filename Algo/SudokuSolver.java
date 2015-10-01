3b55a69385764ce2844e2eb5e608135995e66b0a
payamrastogi <payamrastogi@gmail.com>
2015-10-01 02:12:46 -0400

    Added

--- Algo/SudokuSolver.java
+++ Algo/SudokuSolver.java
@@ -2,58 +2,22 @@
 
 class SudokuSolver
 {
-	private static final int N = 9;
-	private char[][] sudoku;
-	public SudokuSolver()
+	public void solveSudoku(char[][] board) {
+        int N = board.length;
+        solve(board, N);
+    }
+    
+    private boolean isSafe(int row, int col, char c, char[][] sudoku, int N)
 	{
-		sudoku = new char[N][N];
-		//empty cells are indicated by '.'
-		for(int r=0;r<N;r++)
-			for(int c=0;c<N;c++)
-				sudoku[r][c] = '.';
-		
-		sudoku[0][0] = '5';
-		sudoku[0][1] = '3';
-		sudoku[0][4] = '7';
-		sudoku[1][0] = '6';
-		sudoku[1][3] = '1';
-		sudoku[1][4] = '9';
-		sudoku[1][5] = '5';
-		sudoku[2][1] = '9';
-		sudoku[2][2] = '8';
-		sudoku[2][7] = '6';
-		sudoku[3][0] = '8';
-		sudoku[3][4] = '6';
-		sudoku[3][8] = '3';
-		sudoku[4][0] = '4';
-		sudoku[4][3] = '8';
-		sudoku[4][5] = '3';
-		sudoku[4][8] = '1';
-		sudoku[5][0] = '7';
-		sudoku[5][4] = '2';
-		sudoku[5][8] = '6';
-		sudoku[6][1] = '6';
-		sudoku[6][6] = '2';
-		sudoku[6][7] = '8';
-		sudoku[7][3] = '4';
-		sudoku[7][4] = '1';
-		sudoku[7][5] = '9';
-		sudoku[7][8] = '5';
-		sudoku[8][4] = '8';
-		sudoku[8][7] = '7';
-		sudoku[8][8] = '9';
-		//sudoku = ;//initialize sudoku
-	}
-
-	private boolean isSafe(int row, int col, char c)
-	{
-		if(isSafeRow(row, col, c) && isSafeCol(row, col, c) && isSafeBox(row, col, c))
+		if(isSafeRow(row, col, c, sudoku, N) && isSafeCol(row, col, c, sudoku, N) && isSafeBox(row, col, c, sudoku, N))
 			return true;
 		return false;
 	}
 
-	private boolean isSafeRow(int row, int col, char c)
+	private boolean isSafeRow(int row, int col, char c, char[][] sudoku, int N)
 	{
+		//printSudoku(sudoku);
+		//System.out.println("isSafeRow row: "+row+" col: "+col+" char: "+ c);
 		for(int i=0;i<N;i++)
 		{
 			if(sudoku[row][i]==c)
@@ -62,9 +26,11 @@ class SudokuSolver
 		return true;
 	}
 
-	private boolean isSafeCol(int row, int col, char c)
+	private boolean isSafeCol(int row, int col, char c, char[][] sudoku, int N)
 	{
-		for(int i= 0;i<N;i++)
+		//printSudoku(sudoku);
+		//System.out.println("isSafeCol row: "+row+" col: "+col+" char: "+ c);
+		for(int i=0;i<N;i++)
 		{
 			if(sudoku[i][col]==c)
 				return false;
@@ -72,8 +38,10 @@ class SudokuSolver
 		return true;
 	}
 
-	private boolean isSafeBox(int row, int col, char a)
+	private boolean isSafeBox(int row, int col, char a, char[][] sudoku, int N)
 	{
+		//printSudoku(sudoku);
+		//System.out.println("isSafeBox row: "+row+" col: "+col+" char: "+ a);
 		int boxRow = row - row%3;
 		int boxCol = col - col%3;
 
@@ -88,7 +56,7 @@ class SudokuSolver
 		return true;
 	}
 
-	private boolean findUnassigned(int[] empty)
+	private boolean findUnassigned(int[] empty, char[][] sudoku, int N)
 	{
 		empty[0] = -1;
 		empty[1] = -1;
@@ -107,19 +75,19 @@ class SudokuSolver
 		return false;
 	}
 
-	public boolean solve()
+	public boolean solve(char[][] sudoku, int N)
 	{
 		int[] empty = new int[2];
-		if(!findUnassigned(empty))
+		if(!findUnassigned(empty, sudoku, N))
 			return true;
 		int row = empty[0];
 		int col = empty[1];
 		for(char i='1';i<='9';i++)
 		{
-			if(isSafeRow(row, col, i))
+			if(isSafe(row, col, i, sudoku, N))
 			{
 				sudoku[row][col] = i;
-				if(solve())
+				if(solve(sudoku, N))
 					return true;
 				sudoku[row][col] = '.';
 			}
@@ -127,22 +95,30 @@ class SudokuSolver
 		return false;
 	}
 
-	private void printSudoku()
+	private void printSudoku(char[][] board)
 	{
-		for(int i=0;i<N;i++)
+		for(int i=0;i<board.length;i++)
 		{
-			for(int j=0;j<N;j++)
-				System.out.print(sudoku[i][j]+" ");
+			for(int j=0;j<board.length;j++)
+				System.out.print(board[i][j]+" ");
 			System.out.println();
 		}
-		
 	}
 
 	public static void main(String args[])
 	{	
+		String[] s = {"..9748...","7........",".2.1.9...","..7...24.",".64.1.59.",".98...3..","...8.3.2.","........6","...2759.."};
+		char[][] board = new char[s.length][s.length];
+		for(int i=0;i<s.length;i++)
+		{
+			for(int j=0;j<s[i].length();j++)
+			{
+				board[i][j] = s[i].charAt(j);
+			}
+		}
 		SudokuSolver ss = new SudokuSolver();
-		ss.solve();
-		ss.printSudoku();
+		ss.solveSudoku(board);
+		ss.printSudoku(board);
 	}
 
 }
