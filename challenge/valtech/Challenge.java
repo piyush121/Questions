02bffa28a4deb0165d6b1b095c7fb4368d1658ed
Jay Patel <jaympatel567@gmail.com>
2015-10-17 02:11:20 -0400

    valtech program added

--- challenge/valtech/Challenge.java
+++ challenge/valtech/Challenge.java
@@ -13,12 +13,18 @@
 import java.util.Set;
 import java.util.HashSet;
 import java.io.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.security.*;
 
 class Challenge
 {
     
     private Set<String> setOfWords;
     private Set<Character> setOfCharacter;
+    private Map<Character, Integer> characterCountMap;
 
     public Challenge(String text)
     {
@@ -28,6 +34,7 @@ class Challenge
         {
             this.setOfCharacter.add(text.charAt(i));
         }
+        characterCountMap = createHashMap(text);
     }
 
     private void createCharacterSet(String str)
@@ -66,17 +73,122 @@ class Challenge
         return true;
     }
 
-   // private String findPermutation(String str)
+    public Map<Character, Integer> createHashMap(String str)
     {
+        Map<Character, Integer> characterCountMap = new HashMap<>();
+        for (int i=0; i<str.length();i++)
+        {
+            if(str.charAt(i)==' '){
+                continue;
+            }
+            if(characterCountMap.containsKey(str.charAt(i)))
+            {
+                characterCountMap.put(str.charAt(i),characterCountMap.get(str.charAt(i))+1);
+            }
+            else
+            {
+                characterCountMap.put(str.charAt(i),1);
+            }
+        }
+        return characterCountMap;
+    }
 
+
+    boolean isValidHashMap(String word)
+    {
+        //System.out.println("word:" + word);
+        Map<Character, Integer> countMap = createHashMap(word);
+        if(countMap.size()>this.characterCountMap.size())
+        {
+            return false;
+        }
+        for(char a:this.characterCountMap.keySet())
+        {
+            if(countMap.containsKey(a))
+            {
+                if(countMap.get(a)>this.characterCountMap.get(a))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void filterSet()
+    {
+        Set<String> temp = new HashSet<>();
+        for(String word:setOfWords)
+        {
+            
+            if(this.isValidHashMap(word))
+            {
+                temp.add(word);
+            }
+        }
+        setOfWords.clear();
+        setOfWords.addAll(temp);
+    }
+
+    public void findPermutation()
+    {
 
     }
 
-    public static void main(String[] args) throws IOException
+    public void findPermutationHelper(String combine) throws NoSuchAlgorithmException, UnsupportedEncodingException
+    {
+        for(String s : setOfWords)
+        {
+            // System.out.println(s);
+            // concatenated.add(s);
+            combine = combine+" "+s;
+            if(combine.replaceAll(" ","").length() < 15)
+            {
+                //System.out.println("Combined: "+combine+"     S:"+s);
+                // Map<Character, Integer> tempMap = this.createHashMap(combine);
+                if(this.isValidHashMap(combine))
+                    findPermutationHelper(combine);
+            }
+            if(combine.replaceAll(" ","").length()==15)
+            {
+                // System.out.println("Combined: "+combine+"     S:"+s);
+                // System.out.println(comb85ine+" , "+s);   
+                // Map<Character, Integer> tempMap = this.createHashMap(combine);
+                // //System.out.println(tempMap);
+
+                if(this.isValidHashMap(combine)){
+                    //System.out.println(this.characterCountMap);
+                    System.out.println(combine);
+                    if("d5e847370c4d30b7e7aa8441a8b1303d".equals(this.getMD5(combine)))
+                        System.out.println(combine);
+                }
+            }
+                // System.out.println("Combined: "+combine+"     S:"+s);
+                // String temp = concatenated.remove(concatenated.size()-1);
+                combine = combine.substring(0,combine.length()-s.length()-1);
+                // System.out.println(""+combine);
+
+        }
+    }
+
+    public String getMD5(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException
+    {
+        byte[] bytesOfMessage = text.getBytes("UTF-8");
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] thedigest = md.digest(bytesOfMessage);
+        return new String(thedigest);
+    }
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, UnsupportedEncodingException
     {
         Challenge c = new Challenge("forager dog motto");
         c.filterWords();
         System.out.println(c.setOfWords.size());
+        c.filterSet();
+        System.out.println(c.setOfWords.size());
+        // List<String> wordList = new ArrayList<>();
+        // wordList.addAll(c.setOfWords);
+        c.findPermutationHelper("");
     }
 }
 
