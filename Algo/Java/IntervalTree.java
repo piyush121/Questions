16572206326b074b0f53cd808f96b4511f524c8b
Payam Rastogi <payamrastogi@gmail.com>
2015-11-05 20:01:57 -0500

    Interval Tree

---
+++ Algo/Java/IntervalTree.java
@@ -0,0 +1,102 @@
+/* http://www.geeksforgeeks.org/interval-tree/ */
+public class IntervalTree
+{
+	public ITreeNode insert(ITreeNode root, Interval i)
+	{
+		if(root==null)
+			return new ITreeNode(i);
+
+		if(root.interval.lo > i.lo)
+			root.left = insert(root.left , i);
+		else
+			root.right = insert(root.right, i);
+
+		if(root.max < i.hi)
+			root.max = i.hi;
+
+		return root;
+	}
+
+	public boolean doOverlap(Interval i1, Interval i2)
+	{
+		if((i1.lo <= i2.hi) && (i2.lo <= i1.hi))
+			return true;
+		return false;
+	}
+
+	public Interval searchOverlapping(ITreeNode root, Interval i)
+	{
+		if(root == null)
+			return null;
+
+		if(this.doOverlap(root.interval, i))
+			return root.interval;
+
+		if(root.left!=null && root.left.max >= i.lo)
+			return searchOverlapping(root.left, i);
+		return searchOverlapping(root.right, i);
+	}
+
+	public static void main(String args[])
+	{
+		IntervalTree iTree = new IntervalTree();
+		int intr[][] = {
+						{15, 20}, {10, 30}, 
+						{17, 19}, {5, 20}, 
+						{12, 15}, {30, 40}
+    				 };
+    	ITreeNode root = null;
+    	for(int i=0;i<intr.length;i++)
+	    {
+	    	root = iTree.insert(root, new Interval(intr[i][0], intr[i][1]));
+    	}
+    	iTree.inorder(root);
+
+    	Interval x = new Interval(16, 21);
+    	System.out.println(iTree.searchOverlapping(root, x));
+	}
+
+	public void inorder(ITreeNode root)
+	{
+		if(root.left!=null)
+			inorder(root.left);
+		System.out.println(root);
+		if(root.right!=null)
+			inorder(root.right);
+	}
+}
+class ITreeNode 
+{
+	Interval interval;
+	ITreeNode left;
+	ITreeNode right;
+	int max;
+
+	public ITreeNode(Interval i)
+	{
+		interval = i;
+		max = i.hi;
+	}
+
+	public String toString()
+	{
+		return interval.toString();
+	} 
+}
+class Interval
+{
+	int lo;
+	int hi;
+
+	public Interval(int lo, int hi)
+	{
+		this.lo = lo;
+		this.hi = hi;
+	}
+
+	public String toString()
+	{
+		return "["+this.lo +", "+this.hi+"]";
+	}
+
+}
