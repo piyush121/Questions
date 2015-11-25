00aff7f005816a4197dbe646079d788533ee5186
Anupam Jain <anupam2204.smart@gmail.com>
2015-11-25 10:19:32 +0530

    Corrected comment format for GitToPost project

--- Algo/Java/FixedPoint.java
+++ Algo/Java/FixedPoint.java
@@ -1,5 +1,5 @@
-/* http://www.geeksforgeeks.org/find-a-fixed-point-in-a-given-array/ 
-	or magic index (CTCI)
+/* http://www.geeksforgeeks.org/find-a-fixed-point-in-a-given-array/ */
+/*	or magic index (CTCI)
 */
 
 class FixedPoint
