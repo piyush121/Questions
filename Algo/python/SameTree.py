a87690981e8ddd499c3433505d10994635cc6d02
Anupam Jain <anupam2204.smart@gmail.com>
2015-12-01 21:16:49 +0530

    Same Tree problem solution

---
+++ Algo/python/SameTree.py
@@ -0,0 +1,23 @@
+''' https://leetcode.com/problems/same-tree/ '''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not (p or q):
+            return True
+            
+        if not (p and q):
+            return False
+        
+        return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
