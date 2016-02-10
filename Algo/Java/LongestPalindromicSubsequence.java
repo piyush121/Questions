5c8f3257401878b26846b24b74f39a160b572c23
Payam Rastogi <payamrastogi@gmail.com>
2016-02-10 15:41:54 -0500

    LongestPalindromicSubsequence modified

--- Algo/Java/LongestPalindromicSubsequence.java
+++ Algo/Java/LongestPalindromicSubsequence.java
@@ -18,15 +18,22 @@ class LongestPalindromicSubsequence
 
 	public int helper(String s, int start, int end)
 	{
-		//if only one character 
+		//if start==end, then only one character to compare
+		//terminating condition for the odd length strings
 		if(start==end)
 			return 1;
 
 		//if only two characters are left and both are same
+		//terminating condition for the even length strings
 		if((s.charAt(start) == s.charAt(end)) && (start+1 == end))
 			return 2;
+		//if both the characters at the start and the end are same 
+		// then increment the max length found so far with 2
 		if(s.charAt(start)==s.charAt(end))
 			return helper(s, start+1, end-1)+2;
+		//if the characters at the start and end are not equal 
+		//then compare the character at start with character before end 
+		//compare the character after start with the character at end
 		return Math.max(helper(s, start, end-1), helper(s, start+1, end));
 	}
 
