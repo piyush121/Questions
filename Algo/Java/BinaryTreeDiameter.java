b4fb8a9c2f7d688728fbd5ea32ef66675b5f9cc3
Payam Rastogi <payamrastogi@gmail.com>
2016-02-12 12:39:31 -0500

    BinaryTreeDiameter modified

--- Algo/Java/BinaryTreeDiameter.java
+++ Algo/Java/BinaryTreeDiameter.java
@@ -1,25 +1,40 @@
+/* http://www.geeksforgeeks.org/diameter-of-a-binary-tree/ */
 public class BinaryTreeDiameter
 {
 	int max = -1;
+	//determine the height of the tree
 	public int height(TreeNode root)
 	{
+		//if the root is null 
+		//then is no node in the tree or we have reached the leaf node 
+		//of the tree (terminating condition in recursion)
 		if(root==null)
 			return 0;
+		//the height of the tree is 1 plus maximum of height of the left
+		//subtree and height of right subtree
 		return 1 + Math.max(height(root.left), height(root.right));
 	}
-
+	//the diameter of a tree is the farthest distance between two 
+	//nodes
 	public int diameter(TreeNode root)
 	{
 		if(root==null)
 			return 0;
+		//get the height of the left subtree
 		int leftSubTreeHeight = height(root.left);
+		//get the height of the right subtree
 		int rightSubTreeHeight = height(root.right);
-		int diameter = 1 + leftSubTreeHeight + rightSubTreeHeight;
-		if(max< diameter)
-		{
-			max = diameter;
-		}
-		return max-1;
+
+		//get the left diameter
+		int leftDiameter = diameter(root.left);
+		//get the right diameter
+		int rightDiamter = diameter(root.right);
+		// Return max of following three
+   		// 1) Diameter of left subtree
+   		// 2) Diameter of right subtree
+   		// 3) Height of left subtree + height of right subtree + 1
+		return Math.max(leftSubTreeHeight + rightSubTreeHeight + 1
+					, Math.max(leftDiameter, rightDiamter));
 	}
 
 	public static void main(String args[])
