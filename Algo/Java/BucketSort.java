b04bfc25dfedb95d14f2c1b34a7f36f630557df3
Payam Rastogi <payamrastogi@gmail.com>
2016-02-14 13:54:54 -0500

    BucketSort modified

--- Algo/Java/BucketSort.java
+++ Algo/Java/BucketSort.java
@@ -1,19 +1,25 @@
+/* http://www.geeksforgeeks.org/bucket-sort-2/ */
 import java.util.PriorityQueue;
 import java.util.ArrayList;
 class BucketSort
 {
 	public void bucketSort(double[] num)
 	{
+		//an arraylist of priority queues
 		ArrayList<PriorityQueue<Double>> bucket = new ArrayList<PriorityQueue<Double>>(num.length);
 		for(int i=0;i<num.length;i++)
 		{
 			bucket.add(new PriorityQueue<Double>());
 		}
+		//get index 
+		// (Number * length of an arraylist) mod length of an arraylist
 		for(int i=0;i<num.length;i++)
 		{
 			int index = (int)(num[i]*num.length)%num.length;
 			bucket.get(index).add(num[i]);
 		}
+		//print the bucket
+		System.out.println(bucket);
 	}
 
 	public static void main(String args[])
