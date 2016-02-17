183f44bc97eb5dc13c6fb085fdf6c8706a39da31
Payam Rastogi <payamrastogi@gmail.com>
2016-02-17 16:55:46 -0500

    LevelOrderTraversal modified

--- Algo/Java/LevelOrderTraversal.java
+++ Algo/Java/LevelOrderTraversal.java
@@ -1,3 +1,5 @@
+/* https://leetcode.com/problems/binary-tree-level-order-traversal/ */
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -5,34 +7,51 @@ public class LevelOrderTraversal
 {
 	public List<List<Integer>> levelOrder(TreeNode root)
 	{
+		//ArrayList to save the list of all the nodes on the save level
 		List<List<Integer>> nodeList = new ArrayList<List<Integer>>();
+		//if the root is null
+		//then return the empty list
 		if(root==null)
 		{
 			return nodeList;
 		}
+		//Queue
+		//@current to store all the nodes at same level
 		LinkedList<TreeNode> current = null;
+		//@next to store all the child node at the same level
 		LinkedList<TreeNode> next = new LinkedList<TreeNode>();
+		//initially add root to the next
 		next.add(root);
+		//loop until next is empty
 		while(!next.isEmpty())
 		{
+			//assign next to current
 			current = next;
+			//@b ArrayList to store the value of the nodes
 			ArrayList<Integer> b = new ArrayList<Integer>();
+			//initialize next with new LinkedList
 			next = new LinkedList<TreeNode>();
 			while(!current.isEmpty())
 			{
+				//remove the element of the current queue
 				TreeNode node = current.poll();
+				//add the value of the node in the arraylist
 				b.add(node.val);
+				//if the left node is not null
+				//then add left childnode in next queue
 				if(node.left!=null)
 				{
 					next.add(node.left);
 				}
+				//if the right node is not null
+				//then add the right child node in next queue
 				if(node.right!=null)
 				{
-
 					next.add(node.right);
 				}
 				//System.out.println(b);
 			}
+			//add the arraylist b
 			nodeList.add(b);
 		}
 		return nodeList;
