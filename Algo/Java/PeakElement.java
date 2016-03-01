e4bfa68b3ab009033a946e48de25ea0b4120bd5c
Payam Rastogi <payamrastogi@gmail.com>
2016-03-01 18:58:35 -0500

    PeakElement java

--- Algo/Java/PeakElement.java
+++ Algo/Java/PeakElement.java
@@ -1,11 +1,17 @@
+/* https://leetcode.com/problems/find-peak-element/ */
+
 class PeakElement
 {
 	public static int findPeakElement(int[] num)
 	{
+		//if the length of array is 1 then return 0
 		if(num.length==1)
 		{
 			return 0;
 		}
+		//if the length of the array is 2 
+		//if num[0] > num[1] return num[0]
+		//else return num[1]
 		else if(num.length==2)
 		{
 			if(num[0]>num[1])
@@ -13,12 +19,17 @@ class PeakElement
 			else
 				return 1;
 		}
+		//check boundary elements of the array 
+		//if num[0] > num[1] then return 0
 		if(num[0]>num[1])
 		    return 0;
 		else if(num[num.length-1]>num[num.length-2])
 		    return num.length-1;
+		//loop through the array
 		for(int i=1;i<num.length-1;i++)
 		{
+			//check if num[i] is greater than the element on its left and right
+			//then return i
 			if(num[i]>num[i-1] && num[i]>num[i+1])
 				return i;
 		}
